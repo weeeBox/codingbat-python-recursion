8d1ade1d058ebe3cef0a9ca59b93f19c9a1f5e43
Alex Lementuev <a.lementuev@gmail.com>
2021-01-07 15:25:45 -0500

    Used integer division in docstring

--- recursion1/R05_sumDigits.py
+++ recursion1/R05_sumDigits.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 def sumDigits(n: int) -> int:
     """
     Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the
-    rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+    rightmost digit (126 % 10 is 6), while divide (//) by 10 removes the rightmost digit (126 // 10 is 12).
     
     Examples:
     sumDigits(126) → 9
--- recursion1/R06_count7.py
+++ recursion1/R06_count7.py
@@ -4,8 +4,8 @@ from unittest import TestCase
 def count7(n: int) -> int:
     """
     Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no
-    loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the
-    rightmost digit (126 / 10 is 12).
+    loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (//) by 10 removes the
+    rightmost digit (126 // 10 is 12).
     
     Examples:
     count7(717) → 2
